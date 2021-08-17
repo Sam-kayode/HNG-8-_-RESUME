632d82beccd126b485230cc7802531891c1e8b11
Marvellous Sam-kayode <marvelloussamkayode@gmail.com>
2021-08-17 18:28:36 +0300

    Add files via upload

---
+++ HNG internship SUBMISSION_sam-kayode M.K/compulsory number(2) .js
@@ -0,0 +1,8 @@
+// answer to number 2, this function prints my name to the console
+
+function printName(value) {
+    var name = "My name is " + value
+    return name
+}
+
+console.log(printName("sam-kayode Marvellous"))
